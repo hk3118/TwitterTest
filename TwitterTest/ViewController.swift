7a97ecb233c78aba4eb3df09b1b97b50979b392c
小林 悠人 <kobayashihisato@team-lab.com>
2016-05-31 16:53:48 +0900

    Initial Commit

---
+++ TwitterTest/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TwitterTest
+//
+//  Created by 小林 悠人 on 2016/05/31.
+//  Copyright © 2016年 小林 悠人. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
